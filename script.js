bb299df75954334eacc66b70096d65fd7e10bcdf
Proutum-Ann <liangproutum@gmail.com>
2025-12-17 11:46:44 -0500

    removed levels since it'll be a pain to chart multiple levels

--- script.js
+++ script.js
@@ -2,17 +2,12 @@ const vue_app = Vue.createApp({
       // This automatically imports your movies.json file and puts it into
       //   the variable: movies
       created () {
-            fetch('levels.json').then(response => response.json()).then(json => {
-                  this.level = json
-            })
-
             fetch('lions.json').then(response => response.json()).then(json => {
                   this.lion = json
             })
       },
       data() {
         return {
-            level: [],
             lion: []
       }
     },
