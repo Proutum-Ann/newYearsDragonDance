a42ec0f39aad9042132bac2d41f24b23ab4fc077
Proutum-Ann <154941331+Proutum-Ann@users.noreply.github.com>
2025-12-22 03:22:36 +0000

    trying to make sprite show

--- script.js
+++ script.js
@@ -4,40 +4,49 @@ let spriteData = {};
 let currentKey = null;
 
 const vueApp = Vue.createApp({
-    created () {
-        fetch('avatars.json').then(response => response.json()).then(json => {
-           this.avatars = json
-      }),
-      lion.src = spriteData.sprites.idle;
-   },
-    data() {
-        return {
-            avatars: [],
-            currentAvatar: avatars[0],
-            title: 'New Year\'s Dance'
-        }
-    },
-    computed: {
-        
-    },
-    methods: {
-        keyUp(e){
-            if (e.key.toLowerCase() === currentKey) {
-                lion.src = spriteData.sprites.idle;
-                currentKey = null;
-            }
-        },
-        keyDown(e){
-            if (e.repeat) return;
 
-            const key = e.key.toLowerCase();
+  data() {
+    return {
+      sprites: {},
+      currentState: "idle",
+      currentKey: null
+    };
+  },
 
-            if (spriteData.sprites[key]) {
-                lion.src = spriteData.sprites[key];
-                currentKey = key;
-            }
-        }
-    }     
+  created() {
+    fetch("avatars.json")
+      .then(res => res.json())
+      .then(json => {
+        this.sprites = json.sprites;
+      });
+  },
+
+  computed: {
+    lionSrc() {
+      return this.sprites[this.currentState];
+    }
+  },
+
+  methods: {
+    keyDown(e) {
+      if (e.repeat) return;
+
+      const key = e.key.toLowerCase();
+      this.currentKey = key;
+
+      if (key === "w") this.currentState = "up";
+      else if (key === "s") this.currentState = "down";
+      else if (key === "a") this.currentState = "left";
+      else if (key === "d") this.currentState = "right";
+    },
+
+    keyUp(e) {
+      if (e.key.toLowerCase() === this.currentKey) {
+        this.currentState = "idle";
+        this.currentKey = null;
+      }
+    }
+  }
 });
 
-vueApp.mount('#app');
+vueApp.mount("#app");
